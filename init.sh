5e26625825fa6bd47b052549baeca1fbd48fd6af
Shuoliu Yang <visysl@outlook.com>
2020-12-16 14:49:44 +0800

    add alacritty config

--- init.sh
+++ init.sh
@@ -9,3 +9,5 @@ ln -si ${PWD}/home/.zshrc ~/.zshrc
 ln -si ${PWD}/home/.tmux.conf ~/.tmux.conf
 
 rm -rf ~/.SpaceVim.d && ln -sfi ${PWD}/home/.SpaceVim.d ~/.SpaceVim.d
+
+rm -rf ~/.config/alacritty && ln -sfi ${PWD}/home/.config/alacritty ~/.config/alacritty
--- tplgen/gcmake_qt.sh
+++ tplgen/gcmake_qt.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 file="CMakeLists.txt"
 
