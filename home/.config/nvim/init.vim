30a942a2dbd89290f177961560024ec9dc64b27e
Shuoliu Yang <visysl@outlook.com>
2021-12-19 21:46:11 +0800

    add native neovim config file

---
+++ home/.config/nvim/init.vim
@@ -0,0 +1,124 @@
+" Specify a directory for plugins
+" - For Neovim: stdpath('data') . '/plugged'
+" - Avoid using standard Vim directory names like 'plugin'
+call plug#begin(stdpath('data').'/plugged')
+
+" Make sure you use single quotes
+
+" Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
+"
+
+Plug 'kyazdani42/nvim-web-devicons' " for file icons
+Plug 'kyazdani42/nvim-tree.lua'  " file explorer
+Plug 'romgrk/barbar.nvim'  " tabline
+Plug 'numToStr/Comment.nvim'     " code comment/uncomment
+Plug 'voldikss/vim-floaterm'         " neovim integrated terminal of floating window
+
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}     " syntax based hightlighting
+
+Plug 'nvim-lua/plenary.nvim'
+
+Plug 'neovim/nvim-lspconfig'      " builtin lsp for neovim
+
+Plug 'mfussenegger/nvim-dap'
+
+Plug 'RishabhRD/popfix'
+Plug 'hood/popui.nvim'
+Plug 'nvim-lua/popup.nvim'
+
+Plug 'nvim-telescope/telescope.nvim'         " based on fd binary for advanced features
+Plug 'nvim-telescope/telescope-ui-select.nvim'
+
+Plug 'simrat39/rust-tools.nvim'      " an easy-to-use rust tools based on rust-analyzer, if you use debuggin features, codelldb is required
+
+Plug 'sainnhe/sonokai'                " color themes
+
+""""""""""""""""""" Completion""""""""""""""""""
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
+
+Plug 'hrsh7th/nvim-cmp'
+" Initialize plugin system
+call plug#end()
+
+
+"============================= sonokai theme
+" Important!!
+if has('termguicolors')
+  set termguicolors
+endif
+
+"""""""""""""""""""""""""""line number settings
+set nu
+augroup relative_numbser
+ autocmd!
+ autocmd InsertEnter * :set norelativenumber
+ autocmd InsertLeave * :set relativenumber
+augroup END
+
+syntax enable
+set updatetime=300
+
+" The configuration options should be placed before `colorscheme sonokai`.
+let g:sonokai_style = 'andromeda'
+let g:sonokai_enable_italic = 1
+let g:sonokai_disable_italic_comment = 1
+colorscheme sonokai
+
+"""""""""""""""""""""""" barbar.nvim
+" Move to previous/next
+" nnoremap <silent>    <A-,> :BufferPrevious<CR>
+" nnoremap <silent>    <A-.> :BufferNext<CR>
+" Re-order to previous/next
+" nnoremap <silent>    <A-<> :BufferMovePrevious<CR>
+" nnoremap <silent>    <A->> :BufferMoveNext<CR>
+" Goto buffer in position...
+nnoremap <silent>    <leader>1 :BufferGoto 1<CR>
+nnoremap <silent>    <leader>2 :BufferGoto 2<CR>
+nnoremap <silent>    <leader>3 :BufferGoto 3<CR>
+nnoremap <silent>    <leader>4 :BufferGoto 4<CR>
+nnoremap <silent>    <leader>5 :BufferGoto 5<CR>
+nnoremap <silent>    <leader>6 :BufferGoto 6<CR>
+nnoremap <silent>    <leader>7 :BufferGoto 7<CR>
+nnoremap <silent>    <leader>8 :BufferGoto 8<CR>
+nnoremap <silent>    <leader>9 :BufferLast<CR>
+" Pin/unpin buffer
+nnoremap <silent>    <C-w><C-p> :BufferPin<CR>
+" Close buffer
+nnoremap <silent>    <C-w><C-b> :BufferClose<CR>
+" Wipeout buffer
+"                          :BufferWipeout<CR>
+" Close commands
+"                          :BufferCloseAllButCurrent<CR>
+"                          :BufferCloseAllButPinned<CR>
+"                          :BufferCloseBuffersLeft<CR>
+"                          :BufferCloseBuffersRight<CR>
+" Magic buffer-picking mode
+nnoremap <silent> <C-s>    :BufferPick<CR>
+" Sort automatically by...
+nnoremap <silent> <Space>bb :BufferOrderByBufferNumber<CR>
+nnoremap <silent> <Space>bd :BufferOrderByDirectory<CR>
+nnoremap <silent> <Space>bl :BufferOrderByLanguage<CR>
+nnoremap <silent> <Space>bw :BufferOrderByWindowNumber<CR>
+
+
+""""""""""""""""""""""""""""nvim.telescope:  telescope.nvim must be installed
+nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+
+""""""""""""""""nvim-tree
+noremap <silent><F3> :<C-U>NvimTreeToggle<CR>
+
+""""""""""""""""floaterm
+let g:floaterm_keymap_toggle = '<F7>'
+
+
+"""""""""""""""""" configuration written in Lua """""""""""""""""""""
+lua require('config')
---
+++ home/.config/nvim/lua/config.lua
@@ -0,0 +1,182 @@
+--
+----------------------- All language servers
+local servers = { 'pyright', 'tsserver', 'rust_analyzer'}
+
+----------------------- nvim-tree configuration
+require('nvim-tree').setup({})
+
+-----------------------
+require('Comment').setup()
+
+----------------------- cmp-nvim configuration
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+	-- REQUIRED - you must specify a snippet engine
+	expand = function(args)
+	  vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+	  -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+	  -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+	  -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+	end,
+  },
+  mapping = {
+	['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+	['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+	['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+	['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+	['<C-e>'] = cmp.mapping({
+	  i = cmp.mapping.abort(),
+	  c = cmp.mapping.close(),
+	}),
+	-- Accept currently selected item. If none selected, `select` first item.
+	-- Set `select` to `false` to only confirm explicitly selected items.
+	['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = cmp.config.sources({
+	{ name = 'nvim_lsp' },
+	{ name = 'vsnip' }, -- For vsnip users.
+	-- { name = 'luasnip' }, -- For luasnip users.
+	-- { name = 'ultisnips' }, -- For ultisnips users.
+	-- { name = 'snippy' }, -- For snippy users.
+  }, {
+	{ name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+	{ name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+	{ name = 'path' }
+  }, {
+	{ name = 'cmdline' }
+  })
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+
+for _, lsp in ipairs(servers) do
+require('lspconfig')[lsp].setup {
+  capabilities = capabilities
+}
+end
+
+
+-----------------------------------treesitter highlighting ----------------------------------
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+	enable = true,
+	custom_captures = {
+	  -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+	  ["foo.bar"] = "Identifier",
+	},
+	-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+	-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+	-- Using this option may slow down your editor, and you may see some duplicate highlights.
+	-- Instead of true it can also be a list of languages
+	additional_vim_regex_highlighting = false,
+  },
+}
+
+
+
+
+require'nvim-web-devicons'.setup {
+ -- your personnal icons can go here (to override)
+ -- you can specify color or cterm_color instead of specifying both of them
+ -- DevIcon will be appended to `name`
+ override = {
+  zsh = {
+	icon = "",
+	color = "#428850",
+	cterm_color = "65",
+	name = "Zsh"
+  }
+ };
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ default = true;
+}
+
+require'lspconfig'.pyright.setup{}
+
+
+local nvim_lsp = require('lspconfig')
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  print('on_attach')
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+--
+local servers = { 'pyright', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+	on_attach = on_attach,
+	flags = {
+	  debounce_text_changes = 150,
+	}
+  }
+end
+
+local extension_path = 'C:/Users/ysl/.vscode/extensions/vadimcn.vscode-lldb-1.6.10/adapter/'
+local codelldb_path = extension_path .. 'codelldb.exe'
+local liblldb_path = extension_path .. 'liblldb.dll'
+local rust_tools_opts = {
+	-- ... other configs
+	dap = {
+		adapter = require('rust-tools.dap').get_codelldb_adapter(
+			codelldb_path, liblldb_path)
+	},
+	server={
+		on_attach = on_attach,
+		flags = {
+		  debounce_text_changes = 150,
+		}
+    }
+}
+
+require('rust-tools').setup(rust_tools_opts)
+require'rust-tools.hover_actions'.hover_actions()
+
+vim.ui.select = require"popui.ui-overrider"
