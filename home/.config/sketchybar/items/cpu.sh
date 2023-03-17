b8473ec44a4bc58dab722bfa8f7de3264c3846a6
Shuoliu Yang <visysl@outlook.com>
2023-03-17 20:02:41 +0800

    update sketchybar config and others

--- home/.config/sketchybar/items/cpu.sh
+++ home/.config/sketchybar/items/cpu.sh
@@ -1,36 +1,53 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-sketchybar --add   item          cpu.topproc right                  \
-           --set   cpu.topproc   label.font="$FONT:Semibold:7"      \
-                                 label=CPU                          \
-                                 icon.drawing=off                   \
-                                 width=0                            \
-                                 y_offset=6                         \
-                                 update_freq=5                      \
-                                 background.padding_right=10        \
-                                 script="$PLUGIN_DIR/topproc.sh"    \
-                                                                    \
-           --add   item          cpu.percent right                  \
-           --set   cpu.percent   label.font="$FONT:Heavy:12"        \
-                                 label=CPU                          \
-                                 y_offset=-4                        \
-                                 width=40                           \
-                                 icon.drawing=off                   \
-                                 update_freq=2                      \
-                                 background.padding_right=10        \
-                                                                    \
-           --add   graph         cpu.sys right 100                  \
-           --set   cpu.sys       width=0                            \
-                                 graph.color=$RED                   \
-                                 graph.fill_color=$RED              \
-                                 label.drawing=off                  \
-                                 icon.drawing=off                   \
-                                 background.padding_right=10        \
-                                                                    \
-           --add   graph         cpu.user right 100                 \
-           --set   cpu.user      graph.color=$BLUE                  \
-                                 update_freq=2                      \
-                                 label.drawing=off                  \
-                                 icon.drawing=off                   \
-                                 background.padding_right=10        \
-                                 script="$PLUGIN_DIR/cpu.sh"
+cpu_top=(
+  label.font="$FONT:Semibold:7"
+  label=CPU
+  icon.drawing=off
+  width=0
+  padding_right=15
+  y_offset=6
+)
+
+cpu_percent=(
+  label.font="$FONT:Heavy:12"
+  label=CPU
+  y_offset=-4
+  padding_right=15
+  width=55
+  icon.drawing=off
+  update_freq=2
+  mach_helper="$HELPER"
+)
+
+cpu_sys=(
+  width=0
+  graph.color=$RED
+  graph.fill_color=$RED
+  label.drawing=off
+  icon.drawing=off
+  background.height=30
+  background.drawing=on
+  background.color=$TRANSPARENT
+)
+
+cpu_user=(
+  graph.color=$BLUE
+  label.drawing=off
+  icon.drawing=off
+  background.height=30
+  background.drawing=on
+  background.color=$TRANSPARENT
+)
+
+sketchybar --add item cpu.top right              \
+           --set cpu.top "${cpu_top[@]}"         \
+                                                 \
+           --add item cpu.percent right          \
+           --set cpu.percent "${cpu_percent[@]}" \
+                                                 \
+           --add graph cpu.sys right 75          \
+           --set cpu.sys "${cpu_sys[@]}"         \
+                                                 \
+           --add graph cpu.user right 75         \
+           --set cpu.user "${cpu_user[@]}"
--- home/.config/sketchybar/items/spaces.sh
+++ home/.config/sketchybar/items/spaces.sh
@@ -1,49 +1,59 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15")
 
+# Destroy space on right click, focus space on left click.
+# New space by left clicking separator (>)
+
+sid=0
+spaces=()
 for i in "${!SPACE_ICONS[@]}"
 do
   sid=$(($i+1))
-  sketchybar --add space space.$sid left                                 \
-             --set space.$sid associated_space=$sid                      \
-                              icon=${SPACE_ICONS[i]}                     \
-                              icon.padding_left=22                       \
-                              icon.padding_right=22                      \
-                              icon.highlight_color=0xffE48FA8            \
-                              background.padding_left=-8                 \
-                              background.padding_right=-8                \
-                              background.height=26                       \
-                              background.corner_radius=9                 \
-                              background.color=0xff3C3E4F                \
-                              background.drawing=on                      \
-                              label.drawing=off                          \
-                              click_script="yabai -m space --focus $sid"
+
+  space=(
+    associated_space=$sid
+    icon=${SPACE_ICONS[i]}
+    icon.padding_left=10
+    icon.padding_right=15
+    padding_left=2
+    padding_right=2
+    label.padding_right=20
+    icon.highlight_color=$RED
+    label.font="sketchybar-app-font:Regular:16.0"
+    label.background.height=26
+    label.background.drawing=on
+    label.background.color=$BACKGROUND_2
+    label.background.corner_radius=8
+    label.drawing=off
+    script="$PLUGIN_DIR/space.sh"
+  )
+
+  sketchybar --add space space.$sid left    \
+             --set space.$sid "${space[@]}" \
+             --subscribe space.$sid mouse.clicked
 done
 
-sketchybar --add item space_separator left                               \
-           --set space_separator icon=                                  \
-                                 icon.font="Hack Nerd Font:Regular:16.0" \
-                                 background.padding_left=26              \
-                                 background.padding_right=15             \
-                                 label.drawing=off                       \
-                                 icon.color=$WHITE                       \
-                                                                         \
-           --add       event        window_focus                         \
-           --add       item         system.yabai left                    \
-           --set       system.yabai script="$PLUGIN_DIR/yabai.sh"        \
-                                    icon.font="$FONT:Bold:16.0"          \
-                                    label.drawing=off                    \
-                                    icon.width=30                        \
-                                    icon=$YABAI_GRID                     \
-                                    icon.color=$GREEN                    \
-                                    updates=on                           \
-           --subscribe system.yabai window_focus mouse.clicked           \
-                                                                         \
-           --add item window_title left                                  \
-           --set window_title script="$PLUGIN_DIR/window_title.sh"       \
-                              icon.drawing=off                           \
-                              background.padding_left=0                  \
-                              label.color=$WHITE                         \
-                              label.font="$FONT:Black:12.0"              \
-           --subscribe window_title front_app_switched
+spaces=(
+  background.color=$BACKGROUND_1
+  background.border_color=$BACKGROUND_2
+  background.border_width=2
+  background.drawing=on
+)
+
+separator=(
+  icon=􀆊
+  icon.font="$FONT:Heavy:16.0"
+  padding_left=15
+  padding_right=15
+  label.drawing=off
+  associated_display=active
+  click_script='yabai -m space --create && sketchybar --trigger space_change'
+  icon.color=$WHITE
+)
+
+sketchybar --add bracket spaces '/space\..*/' \
+           --set spaces "${spaces[@]}"        \
+                                              \
+           --add item separator left          \
+           --set separator "${separator[@]}"
