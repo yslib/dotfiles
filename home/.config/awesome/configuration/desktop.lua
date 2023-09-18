d7a86da0bbc66a06b4cc10995247a3cc898c5943
Shuoliu Yang <visysl@outlook.com>
2023-09-19 07:09:16 +0800

    change rofi color & add icomoon font

--- home/.config/awesome/configuration/desktop.lua
+++ home/.config/awesome/configuration/desktop.lua
@@ -36,23 +36,23 @@ awesome.connect_signal("widgets::splash::visibility", function(vis)
 end)
 
 --- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-	c:emit_signal("request::activate", "mouse_enter", { raise = false })
-end)
+-- client.connect_signal("mouse::enter", function(c)
+-- 	c:emit_signal("request::activate", "mouse_enter", { raise = false })
+-- end)
 
 --- Wallpapers
 --- ~~~~~~~~~-
-awful.screen.connect_for_each_screen(function(s)
-	if beautiful.wallpaper then
-		local wallpaper = beautiful.wallpaper
-
-		if type(wallpaper) == "function" then
-			wallpaper = wallpaper(s)
-		end
-
-		gears.wallpaper.maximized(wallpaper, s, false, nil)
-	end
-end)
+-- awful.screen.connect_for_each_screen(function(s)
+-- 	if beautiful.wallpaper then
+-- 		local wallpaper = beautiful.wallpaper
+--
+-- 		if type(wallpaper) == "function" then
+-- 			wallpaper = wallpaper(s)
+-- 		end
+--
+-- 		gears.wallpaper.maximized(wallpaper, s, false, nil)
+-- 	end
+-- end)
 
 --- Flash focus
 --- ~~~~~~~~~~~
--- home/.config/awesome/modules/exit-screen.lua
+++ home/.config/awesome/modules/exit-screen.lua
@@ -12,11 +12,11 @@ lock_screen.init()
 
 --- Icons
 local icon_font = "icomoon bold 45"
-local poweroff_text_icon = ""
-local reboot_text_icon = ""
-local suspend_text_icon = ""
-local exit_text_icon = ""
-local lock_text_icon = ""
+local poweroff_text_icon = ""
+local reboot_text_icon = ""
+local suspend_text_icon = ""
+local exit_text_icon = ""
+local lock_text_icon = ""
 
 local button_bg = beautiful.black
 local button_size = dpi(120)
