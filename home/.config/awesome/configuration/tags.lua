77282c5ba856e4373dffb07a0bfed97908fd1b7f
Shuoliu Yang <visysl@outlook.com>
2023-11-02 10:29:34 +0800

    add more tags

--- home/.config/awesome/configuration/tags.lua
+++ home/.config/awesome/configuration/tags.lua
@@ -6,9 +6,9 @@ local awful = require("awful")
 screen.connect_signal("request::desktop_decoration", function(s)
 	--- Each screen has its own tag table.
 	if s.geometry.width > s.geometry.height then
-		awful.tag({ "1", "2", "3", "4" }, s, awful.layout.layouts[1])
+		awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8" }, s, awful.layout.layouts[1])
 	else
 		-- portrait monitor layouts
-		awful.tag({ "1", "2", "3", "4" }, s, awful.layout.suit.tile.bottom)
+		awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8" }, s, awful.layout.suit.tile.bottom)
 	end
 end)
