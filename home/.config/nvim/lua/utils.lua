1309092ff0387054b9edb38795cfb33b79f73a09
slyang <shuoliuyang>
2025-03-15 11:04:01 +0800

    add

--- home/.config/nvim/lua/utils.lua
+++ home/.config/nvim/lua/utils.lua
@@ -1,11 +1,25 @@
 local M = {}
 
 function M.map_key(mode, lhs, rhs, opts)
-	local options = { noremap = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
+
+
+-- enable copy paste from nvim by ssh connection
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
 
 return M
