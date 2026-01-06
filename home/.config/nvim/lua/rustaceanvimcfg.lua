398e8fa01f281b8208d90112d98009b8dd605610
Shuoliu Yang <visysl@outlook.com>
2026-01-06 21:03:10 +0800

    add rustaceanvim

---
+++ home/.config/nvim/lua/rustaceanvimcfg.lua
@@ -0,0 +1,25 @@
+vim.g.rustaceanvim = {
+    server = {
+        on_attach = function(client, bufnr)
+            local bufopts = { noremap = true, silent = true, buffer = bufnr }
+            local function opt(desc, others)
+                return vim.tbl_extend("force", bufopts, { desc = desc }, others or {})
+            end
+
+            vim.keymap.set('n', '<leader>ca', function() vim.cmd.RustLsp('codeAction') end, opt("Rust Code Actions"))
+            vim.keymap.set('n', 'K', function() vim.cmd.RustLsp({ 'hover', 'actions' }) end, opt("Rust Hover Actions"))
+
+            vim.keymap.set('n', '<space>d', function() vim.cmd.RustLsp({ 'renderDiagnostic' }) end,
+                opt("Rust Render Diagnostic"))
+
+            vim.keymap.set('n', '<space>e', function() vim.cmd.RustLsp({ 'expandMacro' }) end,
+                opt("Rust Expand Macro"))
+
+            vim.keymap.set('n', '<F5>', function() vim.cmd.RustLsp({ 'runnables' }) end,
+                opt("Rust Runnables"))
+
+            vim.keymap.set('n', '<F8>', function() vim.cmd.RustLsp({ 'testables' }) end,
+                opt("Rust Testables"))
+        end,
+    }
+}
