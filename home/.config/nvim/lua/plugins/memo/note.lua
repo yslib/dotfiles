991b1a24154c50fb9ed0e5228178270f39742cd2
Shuoliu Yang <visysl@outlook.com>
2026-05-26 03:57:28 +0000

    delete opencode plugin

--- home/.config/nvim/lua/plugins/memo/note.lua
+++ home/.config/nvim/lua/plugins/memo/note.lua
@@ -28,16 +28,6 @@ return {
       title = "如何使用原生的quickfix list",
       cmd = "",
       tags = { "quickfix", "native" },
-      desc = """
-        设置编译器
-        :compiler gcc
-
-        输出编译信息到文件：
-          > output.txt 2>&1
-
-        用:cfile output.txt 将编译信息加载到 quickfix list 中。
-        或者用:cbuffer output.txt 将当前 buffer 的内容加载到 quickfix list 中。
-        :copen 打开 quickfix list 窗口。
-      """
+      desc = "设置编译器 :compiler gcc 输出编译信息到文件：> output.txt 2>&1 用:cfile output.txt 将编译信息加载到 quickfix list 中 或者用:cbuffer output.txt 将当前 buffer 的内容加载到 quickfix list 中。 :copen 打开 quickfix list 窗口。",
   }
 }
