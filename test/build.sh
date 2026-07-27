cc70044ee8fc2c4049ea93be22cb34590e27dff4
Shuoliu Yang <visysl@outlook.com>
2026-07-27 15:33:40 +0800

    remove unsed assets and test

--- test/build.sh
+++
@@ -1,1 +0,0 @@
-sudo docker build --build-arg "https_proxy=http://127.0.0.1:7897" --progress=plain --network host -t test:ubuntu .
--- test/build_arch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo DOCKER_BUILDKIT=1 docker build -f Dockerfile.archlinux --build-arg "https_proxy=http://127.0.0.1:7897" --progress=plain --network host -t test:archlinux .
